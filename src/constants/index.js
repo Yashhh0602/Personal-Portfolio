93ddba3510c60e16c5a168d43f977a24e743c62f
Yash Vardhan <vardhan.yash265@gmail.com>
2024-02-21 15:04:30 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,7 +4,7 @@ import {
     contact,
     css,
     estate,
-    // python,
+    axh,
     express,
     git,
     github,
@@ -24,7 +24,13 @@ import {
     summiz,
     tailwindcss,
     threads,
-    typescript
+    typescript,
+    meta2,
+    kali,
+    wire,
+    snort,
+    c,
+    java
 } from "../assets/icons";
 
 
@@ -36,31 +42,32 @@ export const skills = [
         type: "Frontend",
     },
     {
-        imageUrl: express,
-        name: "PHP",
+        imageUrl: axh,
+        name: "Python",
         type: "Backend",
     },
     {
-        imageUrl: github,
-        name: "GitHub",
-        type: "Version Control",
+        imageUrl: c,
+        name: "Programming Language",
+        type: "Language",
     },
+    {
+        imageUrl: java,
+        name: "Programming Language",
+        type: "Language",
+    }, 
     {
         imageUrl: html,
         name: "HTML",
         type: "Frontend",
-    },
+    }, 
+
     {
         imageUrl: javascript,
         name: "JavaScript",
         type: "Frontend",
     },
-    {
-        imageUrl: mongodb,
-        name: "MongoDB",
-        type: "Database",
-    },
-   
+
     {
         imageUrl: nodejs,
         name: "Node.js",
@@ -71,19 +78,47 @@ export const skills = [
         name: "React",
         type: "Frontend",
     },
-    // {
-    //     imageUrl: python,
-    //     name: "python",
-    //     type: "Backend",
-    // },
    
     {
-        imageUrl: tailwindcss,
-        name: "Tailwind CSS",
-        type: "Frontend",
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+  
+    {
+        imageUrl: kali,
+        name: "Cyber",
+        type: "OS",
     },
    
+    {
+        imageUrl: meta2,
+        name: "Cyber",
+        type: "Cyber Tool",
+    },
+    {
+        imageUrl: wire,
+        name: "Cyber",
+        type: "Cyber Tool",
+    },
+    {
+        imageUrl: snort,
+        name: "Cyber",
+        type: "Cyber Tool",
+    },
+   
+   
+  
+
+    
+   
 ];
+
 
 export const experiences = [
     {
@@ -143,56 +178,72 @@ export const socialLinks = [
     {
         name: 'GitHub',
         iconUrl: github,
-        link: 'https://github.com/YourGitHubUsername',
+        link: 'https://github.com/CodinWithJesus',
     },
     {
         name: 'LinkedIn',
         iconUrl: linkedin,
-        link: 'https://www.linkedin.com/in/YourLinkedInUsername',
+        link: 'https://www.linkedin.com/in/yashvardhan-khanduja07',
     }
 ];
 
- export const projects = [
-     {
-         iconUrl: pricewise,
-         theme: 'btn-back-red',
-        name: 'Amazon Price Tracker',
-         description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
-         link: 'https://github.com/adrianhajdin/pricewise',
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'Chat Bot',
+        description: 'I developed a chatbot leveraging OpenAIs advanced natural language processing capabilities, providing users with interactive and intelligent conversational experiences.',
+        
     },
-     {
-         iconUrl: threads,
-         theme: 'btn-back-green',
-         name: 'Full Stack Threads Clone',
-         description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
-         link: 'https://github.com/adrianhajdin/threads',
-     },
-     {
-         iconUrl: car,
-         theme: 'btn-back-blue',
-         name: 'Car Finding App',
-        description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
-         link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
-     },
-     {
-         iconUrl: snapgram,
-         theme: 'btn-back-pink',
-         name: 'Full Stack Instagram Clone',
-         description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
-         link: 'https://github.com/adrianhajdin/social_media_app',
-     },
-     {
-         iconUrl: estate,
-         theme: 'btn-back-black',
-         name: 'Real-Estate Application',
-         description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
-         link: 'https://github.com/adrianhajdin/projects_realestate',
-     },
-     {
-         iconUrl: summiz,
-         theme: 'btn-back-yellow',
-         name: 'AI Summarizer Application',
-         description: 'App that leverages AI to automatically generate concise & informative summaries from lengthy text content, or blogs.',
-         link: 'https://github.com/adrianhajdin/project_ai_summarizer',
-     }
- ];
+    {
+        iconUrl: threads,
+        theme: 'btn-back-green',
+        name: '3D-Game',
+        description:'I have crafted a captivating 3D game using Unity, bringing immersive experiences to life through intricate design and dynamic gameplay mechanics, all powered by C# scripting.'
+    },
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-blue',
+        name: 'to-do list',
+        description: 'I have developed a versatile to-do list application in JavaScript, allowing users to efficiently manage tasks by seamlessly tracking both completed and pending items, enhancing productivity and organization.',
+        
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Mobile App',
+        description: 'I created a simple and user-friendly e-commerce mobile app using React, making shopping online hassle-free and enjoyable for everyone.'
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'College Attendance Portal',
+        description:'I designed a College attendance portal integrating MySQL database, facilitating efficient tracking and management of student attendance, ensuring accurate record-keeping and streamlined administrative processes.'
+        
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'AI Hand Tracking',
+        description: 'I developed an AI hand tracking system utilizing OpenCV, enabling real-time detection and tracking of hand gestures and movements.'
+    },
+
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-blue',
+        name: 'AI Face Tracking',
+        description: 'I created an AI face tracking system using OpenCV, allowing for accurate real-time detection and tracking of faces, enabling diverse applications in security, surveillance, and augmented reality experiences.'
+    },
+    {
+        iconUrl: snapgram3,
+        theme: 'btn-back-green',
+        name: 'Real-Time Chat Application',
+        description: 'Developed a real-time chat application using MERN stack, enabling seamless communication between users. Utilized WebSockets for instant message delivery and provided a responsive and intuitive user interface'
+    },
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-yellow',
+        name: 'Full stack application',
+        description: ''
+    }
+];
